2e206b1d0fed08f27a88feb81c49c54a5bdcc58d
OmarAvelar <44375609+OmarAvelar@users.noreply.github.com>
2023-10-11 19:09:00 -0600

    Update main.tsx

--- auth-front/src/main.tsx
+++ auth-front/src/main.tsx
@@ -37,6 +37,10 @@ const router = createBrowserRouter([
         path: "/processing",
         element: <Processing />,
       },
+      {
+        path: "/api/upload/arg",
+        element: <Processing />,
+      },
     ],
   },
 ]);
